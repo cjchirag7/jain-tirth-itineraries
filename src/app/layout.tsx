118bdbab0db22fe5b3bb4ae080c5ac2389855c6c
cjchirag7 <cjchirag7@gmail.com>
2026-03-07 22:34:00 +0530

    Add Google analytics and suggestions email in footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Jain Routes | Plan Your Tirth Yatra",
@@ -17,6 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F2MGPTFDDH" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-F2MGPTFDDH');
+          `}
+        </Script>
+      </head>
       <body>
         <Header />
         <main>{children}</main>
